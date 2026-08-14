91e75e506d255d7c08bb0323eb34823bedb00a6a
Leonardo Rebouças <leoabcblm@gmail.com>
2026-08-13 22:43:24 -0300

    feat: add Electron desktop shell

---
+++ dist-electron/main.js
@@ -0,0 +1,24 @@
+import { BrowserWindow, app } from "electron";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+//#region electron/main.ts
+var __dirname = path.dirname(fileURLToPath(import.meta.url));
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1280,
+		height: 800,
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+			contextIsolation: true,
+			nodeIntegration: false
+		}
+	});
+	if (process.env.VITE_DEV_SERVER_URL) win.loadURL(process.env.VITE_DEV_SERVER_URL);
+	else win.loadFile(path.join(__dirname, "../dist/index.html"));
+}
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
+//#endregion
+export {};
